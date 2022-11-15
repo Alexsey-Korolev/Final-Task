00e70d85ee533a2eee42bb562f6357357d0f5f2e
Aleksei <spacerabbit1@mail.ru>
2022-11-15 11:45:56 +0700

    добавлен код

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] arrayOne = new string[5] {"345", "6", "hello", "world", "sam"};
+string[] arrayTwo = new string[arrayOne.Length];
+void SecondArrayWithIF(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+    if(arrayOne[i].Length <= 3)
+        {
+        arrayTwo[count] = arrayOne[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
